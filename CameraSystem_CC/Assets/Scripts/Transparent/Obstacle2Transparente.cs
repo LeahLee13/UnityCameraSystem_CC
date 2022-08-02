e7bd4ae6659ec1f91424f7993d12455275df6f31
leah13 <leah13@163.com>
2022-08-02 19:21:49 +0800

    修复遮挡透明Bug

--- CameraSystem_CC/Assets/Scripts/Transparent/Obstacle2Transparente.cs
+++ CameraSystem_CC/Assets/Scripts/Transparent/Obstacle2Transparente.cs
@@ -12,8 +12,8 @@ public class Obstacle2Transparente : MonoBehaviour
     private struct HitInfo
     {
         public GameObject obj;
-        public Renderer renderer;
-        public Material material;
+        public Renderer[] renderers;
+        public List<Material> materials;
     }
 
     void Update()
@@ -32,6 +32,7 @@ public class Obstacle2Transparente : MonoBehaviour
         //用相机位置与角色位置的差值-1是为了避免检测到地面，提高可移植性，否则射线检测到的对象还要除去地面
         hits = new List<RaycastHit>(Physics.RaycastAll(transform.position, transform.forward, Vector3.Distance(this.transform.position, player.transform.position) - 1));
 
+        //替换材质
         for (int i = 0; i < hits.Count; i++)
         {
             //射线检测到的对象除去角色
@@ -39,20 +40,39 @@ public class Obstacle2Transparente : MonoBehaviour
             {
                 var hit = hits[i];
                 int findIndex = changedInfos.FindIndex(item => item.obj == hit.collider.gameObject);
+                var rendArray = hit.collider.gameObject.GetComponentsInChildren<Renderer>();
 
-                //没找到则添加
-                if (findIndex < 0)
+                if (rendArray.Length > 0)
                 {
-                    var changed = new HitInfo();
-                    changed.obj = hit.collider.gameObject;
-                    changed.renderer = changed.obj.GetComponent<Renderer>();
-                    changed.material = changed.renderer.material;
-                    changed.renderer.material = alphaMaterial;
-                    changedInfos.Add(changed);
+                    //没找到则添加
+                    if (findIndex < 0)
+                    {
+                        var changed = new HitInfo();
+                        changed.obj = hit.collider.gameObject;
+                        changed.renderers = rendArray;
+                        changed.materials = new List<Material>();
+
+                        for (int j = 0; j < rendArray.Length; j++)
+                        {
+                            var materials = rendArray[j].materials;
+                            var tempMaterials = new Material[materials.Length];
+
+                            for (int k = 0; k < materials.Length; k++)
+                            {
+                                changed.materials.Add(materials[k]);
+                                tempMaterials[k] = alphaMaterial;
+                            }
+
+                            rendArray[j].materials = tempMaterials;//替换材质
+                        }
+
+                        changedInfos.Add(changed);
+                    }
                 }
             }
         }
 
+        //还原材质
         for (int i = 0; i < changedInfos.Count;)
         {
             var changedInfo = changedInfos[i];
@@ -61,7 +81,14 @@ public class Obstacle2Transparente : MonoBehaviour
             //没找到则移除
             if (findIndex < 0)
             {
-                changedInfo.renderer.material = changedInfo.material;//还原材质
+                if (changedInfo.obj != null)
+                {
+                    foreach (var renderer in changedInfo.renderers)
+                    {
+                        renderer.materials = changedInfo.materials.ToArray();//还原材质
+                    }
+                }
+
                 changedInfos.RemoveAt(i);
             }
             else
@@ -71,6 +98,3 @@ public class Obstacle2Transparente : MonoBehaviour
         }
     }
 }
-
-
-
